da22e0b3e5745355f9e24240e1c7cf3cc17531c5
hvkcoder <hvkcoder@163.com>
2019-01-07 22:55:33 +0800

    Add the clock draw

--- canvas/the-clock/assets/js/app.js
+++ canvas/the-clock/assets/js/app.js
@@ -3,6 +3,24 @@
 (function (document) {
     var canvas = document.querySelector('#canvas');
     var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;
+    var r = width / 2;
 
+    console.log(r);
 
+    // 绘制 表盘
+    function drawDial() {
+        ctx.save();
+        ctx.beginPath();
+        ctx.lineWidth = 10;
+        ctx.arc(r, r, r - 5, 0, 2 * Math.PI, false);
+        ctx.stroke();
+    }
+
+    // 绘制时钟刻度
+    function drawClock() {
+    }
+
+    drawDial();
 })(document);
